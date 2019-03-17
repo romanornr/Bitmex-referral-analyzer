65ab781ab2334ca90f4ee1875e27cef2bf9c29e6
Romano <romanornr@gmail.com>
2019-03-17 14:20:35 +0100

    Basic ref fee calculation

---
+++ main.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type Transaction struct {
+	Time          string
+	Type          string
+	Amount        float64
+	Fee           float64
+	Address       string
+	Status        string
+	WalletBalance string
+}
+
+type Month int
+
+const (
+	JAN Month = iota + 1
+	FEB
+	MAR
+	APR
+	MAY
+	JUN
+	AUG
+	SEPT
+	OCT
+	NOV
+	DEC
+)
+
+func (month Month) referralEarning(transactions []Transaction) {
+	var earnedBTC float64
+	var count int
+	//t := time.Month(2)
+	//fmt.Println(t)
+
+	for _, tx := range transactions {
+		if tx.Type == "AffiliatePayout" {
+			earnedBTC += (tx.Amount / 100000000)
+			count += 1
+		}
+	}
+	fmt.Printf("earned ref fees for %d : %f BTC\n", month, earnedBTC)
+}
+
+type Months struct {
+	Jan  Month
+	Feb  Month
+	Mar  Month
+	Apr  Month
+	May  Month
+	Jun  Month
+	Jul  Month
+	Aug  Month
+	Sept Month
+	Oct  Month
+	Nov  Month
+	Dec  Month
+}
+
+func ScanFiles(extension string) string {
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		if !f.IsDir() {
+			r, err := regexp.MatchString(extension, f.Name())
+			if err == nil && r {
+				//return nil
+				return f.Name()
+			}
+		}
+	}
+	os.Exit(0)
+	return ""
+}
+
+func ReadCSV(file string) ([]Transaction, error) {
+
+	var transactions []Transaction
+	csvFile, _ := os.Open("./" + file)
+	r := csv.NewReader(bufio.NewReader(csvFile))
+
+	r.LazyQuotes = true
+	r.Comma = ','
+
+	_, err := r.Read()
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+
+	for {
+		record, error := r.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
+		}
+
+		amount, _ := strconv.ParseFloat(record[2], 64)
+		fee, _ := strconv.ParseFloat(record[3], 64)
+
+		tx := Transaction{
+			Time:          record[0],
+			Type:          record[1],
+			Amount:        amount,
+			Fee:           fee,
+			Address:       record[4],
+			Status:        record[5],
+			WalletBalance: record[6],
+		}
+
+		transactions = append(transactions, tx)
+	}
+
+	return transactions, nil
+}
+
+func calculateTotalReferral(transactions []Transaction) {
+
+	var earned float64
+	var m Months
+	var transactionsJan []Transaction
+	var transactionsFeb []Transaction
+	var transactionsMar []Transaction
+	//var transactionsApr []Transaction
+	//var transactionsMay []Transaction
+	//var transactionsJun []Transaction
+	//var transactionsJul []Transaction
+	//var transactionsAug []Transaction
+	//var transactionsSept []Transaction
+	//var transactionsOct []Transaction
+	//var transactionsNov []Transaction
+	//var transactionsDec []Transaction
+
+	for _, tx := range transactions {
+		if tx.Type == "AffiliatePayout" {
+			earned += tx.Amount / 100000000
+		}
+
+		time := strings.Split(tx.Time, ",")
+		date := strings.Split(time[0], "/")
+		month, _ := strconv.Atoi(date[0])
+
+		switch month {
+		case 1:
+			transactionsJan = append(transactionsJan, tx)
+		case 2:
+			transactionsFeb = append(transactionsFeb, tx)
+		case 3:
+			transactionsMar = append(transactionsMar, tx)
+		}
+
+	}
+
+	m.Jan.referralEarning(transactionsJan)
+	m.Feb.referralEarning(transactionsFeb)
+	m.Mar.referralEarning(transactionsMar)
+
+	fmt.Printf("Total earned ref fees: %f BTC\n", earned)
+}
+
+func main() {
+	file := ScanFiles(".csv")
+	transactions, _ := ReadCSV(file)
+	calculateTotalReferral(transactions)
+}
